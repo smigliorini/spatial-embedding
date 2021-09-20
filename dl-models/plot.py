f732848a5b612e7becea8d95dfae231effb29f8a
albertounivr <alberto.belussi@univr.it>
2021-09-20 17:12:45 +0200

    Update plot.py
     Added new function plot_h1

--- dl-models/plot.py
+++ dl-models/plot.py
@@ -18,6 +18,25 @@ def plot_h(orig,dec,start,n):
     ax.get_xaxis().set_visible(False)
     ax.get_yaxis().set_visible(False)
   plt.show()
+def plot_h1(orig,dec,start,n):
+  plt.figure(figsize=(20, 4))
+  for i in range(n):
+    # display original
+    ax = plt.subplot(2, n, i + 1)
+    plt.imshow(orig[start+i])
+    plt.title("original ["+str(start+i)+"]")
+    plt.gray()
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+
+    # display reconstruction
+    ax = plt.subplot(2, n, i + 1 + n)
+    plt.imshow(dec[start+i])
+    plt.title("reconstructed")
+    plt.gray()
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+  plt.show()
 def plot_h6(orig,dec,start,n):
   plt.figure(figsize=(20, 8))
   for i in range(n):
