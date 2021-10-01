685fa0a8d92dd8fd79fda3ad47c2580e92142f21
albertounivr <alberto.belussi@univr.it>
2021-10-01 13:55:21 +0200

    modified the creation of the CNN network: there was an error in the choice of the latent_dim, i.e. of the embedding size

--- dl-models/model0.py
+++ dl-models/model0.py
@@ -14,10 +14,6 @@ import myAutoencoder as enc
 # b: rq selectivity
 # r: range query histogram
 def create_h(n,dimx,dimy,dimz,norm_type,distr):
-	#n = 2500
-	#dimx = 128
-	#dimy = 128
-	#dimz = 6
 	print("Generating ", n, " random histograms (",dimx,"x",dimy,"x",dimz,") ...")
 	if (norm_type == 0):
 		print("Method for reducing the range of the feature values: standardization")
@@ -50,16 +46,17 @@ def create_h(n,dimx,dimy,dimz,norm_type,distr):
 
 # Autoencoder
 def auto_encoder(type,loc,glo,dimx,dimy,dimz,ldim_loc,ldim_glo,a,g,b,r):
-	#dimx = 128
-	#dimy = 128
-	#dimz = 6
+	# latent_dim is equal to the size of the embedding representation of the histogram for DENSE network
+	# while for CNN it is equal to the number of values in each cell of the embedding, thus the size of
+	# the embedding is obtained by the formula: dimx/4 * dimy/4 * latent_dim
 	if (loc == 1):
 		latent_dim = ldim_loc
-		print("Embedding dim: ", latent_dim)
 		if (type == 0):
+			print("Embedding dim: ", latent_dim)
 			print("Inizilizing Autoencoder DENSE local...")
 			ae_loc = enc.Autoencoder_local(latent_dim,dimx,dimy,dimz)
 		else:
+			print("Embedding dim: ", latent_dim*dimx/4*dimy/4)
 			print("Inizilizing Autoencoder CNN local...")
 			ae_loc = enc.AutoencoderCNN_local(latent_dim,dimx,dimy,dimz)
 		ae_loc.compile(optimizer='adam', loss=losses.MeanSquaredError())
@@ -71,18 +68,19 @@ def auto_encoder(type,loc,glo,dimx,dimy,dimz,ldim_loc,ldim_glo,a,g,b,r):
 		X_train_hist, X_test_hist, X_train_range, X_test_range, y_train_card, y_test_card = train_test_split(a, r, b, test_size=0.2)
 		# training
 		print("Training Autoencoder local...")
-		ae_loc.fit(X_train_hist, X_train_hist, batch_size=64, epochs=20, shuffle=True, validation_data=(X_test_hist, X_test_hist))
+		ae_loc.fit(X_train_hist, X_train_hist, batch_size=16, epochs=20, shuffle=True, validation_data=(X_test_hist, X_test_hist))
 	else:
 		print("Skip local autoencoder")
 	if (glo == 1):
 		latent_dim = ldim_glo
-		print("Embedding dim: ", latent_dim)
 		if (type == 0):
+			print("Embedding dim: ", latent_dim)
 			print("Inizilizing Autoencoder DENSE global...")
 			ae_glo = enc.Autoencoder_global(latent_dim,dimx,dimy)
 		else:
+			print("Embedding dim: ", latent_dim*dimx/4*dimy/4)
 			print("Inizilizing Autoencoder CNN global...")
-			ae_glo = enc.AutoencoderCNN_global(latent_dim,dimx,dimyi,1)
+			ae_glo = enc.AutoencoderCNN_global(latent_dim,dimx,dimy,1)
 		ae_glo.compile(optimizer='adam', loss=losses.MeanSquaredError())
 		#ae_glo.compile(optimizer='adam', loss='binary_crossentropy')
 		# splitting train and test 0.2
@@ -91,7 +89,7 @@ def auto_encoder(type,loc,glo,dimx,dimy,dimz,ldim_loc,ldim_glo,a,g,b,r):
 		X_train_global, X_test_global = train_test_split(g, test_size=0.2)
 		# training
 		print("Training Autoencoder global...")
-		ae_glo.fit(X_train_global, X_train_global, batch_size=32, epochs=20, shuffle=True, validation_data=(X_test_global, X_test_global))
+		ae_glo.fit(X_train_global, X_train_global, batch_size=32, epochs=12, shuffle=True, validation_data=(X_test_global, X_test_global))
 	else:
 		print("Skip global autoencoder")
 	if (loc == 1):
--- dl-models/run_model1.py
+++ dl-models/run_model1.py
@@ -1,10 +1,10 @@
 import model0 as m
-# DENSE NETWORK
+# CNN NETWORK
 a,g,b,r = m.create_h(2500,128,128,6,1,5)
 # local histograms
-autoenc_a, a_train, a_test = m.auto_encoder(0,1,0,128,128,6,2048,128,a,g,b,r)
+autoenc_a, a_train, a_test = m.auto_encoder(1,1,0,128,128,6,12,128,a,g,b,r)
 # global histograms
-autoenc_g, g_train, g_test = m.auto_encoder(0,0,1,128,128,6,2048,128,a,g,b,r)
+autoenc_g, g_train, g_test = m.auto_encoder(1,0,1,128,128,6,12,128,a,g,b,r)
 # enc dec global
 enc_g = autoenc_g.encoder(g).numpy()
 dec_g = autoenc_g.decoder(enc_g).numpy()
@@ -17,10 +17,10 @@ enc_a_test = autoenc_a.encoder(a_test).numpy()
 dec_a_test = autoenc_a.decoder(enc_a_test).numpy()
 # plot results for local histograms
 import plot as p
-p.plot_h6_f(a_test, dec_a_test,0,10,'t1.png')
-p.plot_h6_f(a_test, dec_a_test,100,10, 't2.png')
-p.plot_h6_f(a_test, dec_a_test,250,10, 't3.png')
-p.plot_h6_f(a_test, dec_a_test,350,10, 't4.png')
+p.plot_h6_f(a_test, dec_a_test,0,10,'t1_cnn.png')
+p.plot_h6_f(a_test, dec_a_test,100,10, 't2_cnn.png')
+p.plot_h6_f(a_test, dec_a_test,250,10, 't3_cnn.png')
+p.plot_h6_f(a_test, dec_a_test,350,10, 't4_cnn.png')
 # plot results for global histograms
-p.plot_h1_f(g_test, dec_g_test,0,10, 'tg1.png')
-p.plot_h1_f(g_test, dec_g_test,100,10, 'tg2.png')
+p.plot_h1_f(g_test, dec_g_test,0,10, 'tg1_cnn.png')
+p.plot_h1_f(g_test, dec_g_test,100,10, 'tg2_cnn.png')
