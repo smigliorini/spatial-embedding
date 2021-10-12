5ddb6bf9e29d037d3415c74332dffbb1611a8406
albertounivr <alberto.belussi@univr.it>
2021-10-12 15:52:06 +0200

    Added new functions for plotting a histogram

--- dl-models/plot.py
+++ dl-models/plot.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import numpy as np
 def plot_freq(f):
   max = 0
   for k in range(f.shape[0]):
@@ -32,6 +33,34 @@ def plot_orig_scale(s,orig,start,n):
     # display original
     ax = plt.subplot(2, n, i + 1)
     plt.imshow(orig[start+i]*s)
+    plt.title("original ["+str(start+i)+"]")
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+  plt.show()
+def plot_orig_scale_auto(orig,start,n):
+  plt.figure(figsize=(20, 4))
+  for i in range(n):
+    # display original
+    max = np.array([0.0,0.0,0.0])
+    for j in range(orig[start+i].shape[0]):
+      for k in range(orig[start+i].shape[1]):
+        if (orig[start+i,j,k,0] > max[0]):
+          max[0] = orig[start+i,j,k,0]
+        if (orig[start+i,j,k,1] > max[1]):
+          max[1] = orig[start+i,j,k,1]
+        if (orig[start+i,j,k,2] > max[2]):
+          max[2] = orig[start+i,j,k,2]
+    norm = np.zeros((orig.shape[1],orig.shape[2],3))
+    for j in range(orig[start+i].shape[0]):
+      for k in range(orig[start+i].shape[1]):
+        if (max[0] > 0.0):
+           norm[j,k,0] = orig[start+i,j,k,0]/max[0]
+        if (max[1] > 0.0):
+           norm[j,k,1] = orig[start+i,j,k,1]/max[1]
+        if (max[2] > 0.0):
+           norm[j,k,2] = orig[start+i,j,k,2]/max[2]
+    ax = plt.subplot(2, n, i + 1)
+    plt.imshow(norm)
     plt.title("original ["+str(start+i)+"]")
     ax.get_xaxis().set_visible(False)
     ax.get_yaxis().set_visible(False)
@@ -163,3 +192,62 @@ def plot_h6_f(orig,dec,start,n,file):
     ax.get_xaxis().set_visible(False)
     ax.get_yaxis().set_visible(False)
   plt.savefig(file)
+def plot_h6_mix_neg(orig,dec,start,n):
+  plt.figure(figsize=(20, 8))
+  for i in range(n):
+    # display original feature 0,2,4
+    ax = plt.subplot(4, n, i + 1)
+    norm = np.zeros((orig.shape[1],orig.shape[2],3))
+    for j in range(orig.shape[1]):
+      for k in range(orig.shape[2]):
+        norm[j,k,0] = (1-orig[start+i,j,k,0])
+        norm[j,k,1] = (1-orig[start+i,j,k,2])
+        norm[j,k,2] = (1-orig[start+i,j,k,4])
+    plt.imshow(norm)
+    plt.title("orig(0,2,4) ["+str(start+i)+"]")
+    #plt.gray()
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+
+    # display reconstruction feature 0,2,4
+    ax = plt.subplot(4, n, i + 1 + n)
+    denorm = np.zeros((orig.shape[1],orig.shape[2],3))
+    for j in range(orig.shape[1]):
+      for k in range(orig.shape[2]):
+        denorm[j,k,0] = (1-dec[start+i,j,k,0])
+        denorm[j,k,1] = (1-dec[start+i,j,k,2])
+        denorm[j,k,2] = (1-dec[start+i,j,k,4])
+    plt.imshow(denorm)
+    plt.title("reconstructed")
+    #plt.gray()
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+
+    # display original feature 1,3,5
+    ax = plt.subplot(4, n, i + 1 + 2*n)
+    norm1 = np.zeros((orig.shape[1],orig.shape[2],3))
+    for j in range(orig.shape[1]):
+      for k in range(orig.shape[2]):
+        norm1[j,k,0] = (1-orig[start+i,j,k,1])
+        norm1[j,k,1] = (1-orig[start+i,j,k,3])
+        norm1[j,k,2] = (1-orig[start+i,j,k,5])
+    plt.imshow(norm1)
+    plt.title("orig(1,3,5) ["+str(start+i)+"]")
+    #plt.gray()
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+
+    # display reconstruction feature 1,3,5
+    ax = plt.subplot(4, n, i + 1 + 3*n)
+    denorm1 = np.zeros((orig.shape[1],orig.shape[2],3))
+    for j in range(orig.shape[1]):
+      for k in range(orig.shape[2]):
+        denorm1[j,k,0] = (1-dec[start+i,j,k,1])
+        denorm1[j,k,1] = (1-dec[start+i,j,k,3])
+        denorm1[j,k,2] = (1-dec[start+i,j,k,5])
+    plt.imshow(denorm1)
+    plt.title("reconstructed")
+    #plt.gray()
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+  plt.show()
