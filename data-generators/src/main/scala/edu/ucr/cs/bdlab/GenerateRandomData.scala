2de458adb6774025d574e1d507e4e5b2d70cb0b1
Ahmed Eldawy <eldawy@ucr.edu>
2021-10-14 20:38:26 -0700

    Added code to run range queries directly on generated data

--- data-generators/src/main/scala/edu/ucr/cs/bdlab/GenerateRandomData.scala
+++ data-generators/src/main/scala/edu/ucr/cs/bdlab/GenerateRandomData.scala
@@ -15,16 +15,49 @@
  */
 package edu.ucr.cs.bdlab
 
-import edu.ucr.cs.bdlab.beast.generator.SpatialGenerator
+import edu.ucr.cs.bdlab.beast.generator._
+import edu.ucr.cs.bdlab.beast.geolite.EnvelopeNDLite
+import edu.ucr.cs.bdlab.beast.indexing.RSGrovePartitioner
+import edu.ucr.cs.bdlab.beast.io.{CSVFeatureWriter, SpatialWriter}
 import edu.ucr.cs.bdlab.beast.operations.GriddedSummary
+import edu.ucr.cs.bdlab.beast.{SpatialRDD, _}
 import edu.ucr.cs.bdlab.davinci.SingleLevelPlot
+import org.apache.commons.cli.{BasicParser, HelpFormatter, Options}
 import org.apache.hadoop.fs.{Path, PathFilter}
-import org.apache.spark.sql.{SaveMode, SparkSession}
+import org.apache.spark.sql.{DataFrame, SaveMode, SparkSession}
 import org.apache.spark.{SparkConf, SparkContext}
+import org.locationtech.jts.geom.{Envelope, GeometryFactory}
 
+import java.io.PrintStream
 import java.util.concurrent.TimeoutException
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration._
+import scala.concurrent.{Await, Future}
+import scala.util.Random
 
 object GenerateRandomData {
+  val globalMBR = new EnvelopeNDLite(2, 0, 0, 10, 10)
+
+  lazy val commandLineOptions: Options = {
+    new Options()
+      .addOption(new org.apache.commons.cli.Option("o", "output", true,
+        "The path to write all output files"))
+      .addOption(new org.apache.commons.cli.Option("d", "datasets", true,
+        "The path to write the output datasets to (relative to the output directory)"))
+      .addOption(new org.apache.commons.cli.Option("s", "summaries", true,
+        "The path to write the summaries (relative to the output directory)"))
+      .addOption(new org.apache.commons.cli.Option("gs", "global-summary", true,
+        "The path to write the file with global summary of all datasets to (relative to the output directory)"))
+      .addOption(new org.apache.commons.cli.Option("i", "images", true,
+        "The path to write the images (relative to the output directory)"))
+      .addOption(new org.apache.commons.cli.Option("qi", "queries-input", true,
+        "A path to a file that contains all range queries to execute"))
+      .addOption(new org.apache.commons.cli.Option("qo", "queries-output", true,
+        "A path to a file to write the results of range queries to (relative to the output directory)"))
+      .addOption(new org.apache.commons.cli.Option("h", "help", false,
+        "Print this help information"))
+  }
+
   def main(args: Array[String]): Unit = {
     // Create the Spark context
     val conf = new SparkConf
@@ -35,20 +68,152 @@ object GenerateRandomData {
     if (!conf.contains("spark.master"))
       conf.setMaster("local[*]")
 
-    val outPath = new Path(if (args.length == 0) "embedding-assorted-datasets" else args(0))
-
+    val parser = new BasicParser
+    val commandline = parser.parse(commandLineOptions, args)
+    if (commandline.hasOption("help")) {
+      new HelpFormatter().printHelp("data-generator", commandLineOptions)
+    }
     val spark: SparkSession = SparkSession.builder().config(conf).getOrCreate()
     val sc: SparkContext = spark.sparkContext
+    val outPath = new Path(commandline.getOptionValue("output", "embedding-assorted-datasets"))
+    val filesystem = outPath.getFileSystem(sc.hadoopConfiguration)
+    val datasetsPath = new Path(outPath, commandline.getOptionValue("datasets", "datasets"))
+    val summariesPath = new Path(outPath, commandline.getOptionValue("summaries", "summaries"))
+    val imagePath = new Path(outPath, commandline.getOptionValue("images", "images"))
+    val summaryOutput: PrintStream = if (!commandline.hasOption("global-summary")) null else {
+      val globalSummaryPath = new Path(outPath, commandline.getOptionValue("global-summary"))
+      val out = new PrintStream(filesystem.create(globalSummaryPath))
+      out.println("dataset,x1,y1,x2,y2,num_features,size,num_points,avg_area,avg_side_length_0,avg_side_length_1")
+      out
+    }
+    val queriesInput = commandline.getOptionValue("queries-input")
+    val queriesOutput: PrintStream = if (queriesInput == null) null else {
+      val queriesOutputFile = new Path(outPath, commandline.getOptionValue("queries-output", "range-queries-result.csv"))
+      val out = new PrintStream(filesystem.create(queriesOutputFile))
+      out.println("dataset;numQuery;queryArea;areaInt;cardinality;executionTime;mbrTests")
+      out
+    }
+    val rangeQueries: DataFrame = if (queriesInput == null) null else
+      spark.read
+        .option("delimiter", ",")
+        .option("header", true)
+        .option("inferschema", true)
+        .csv(queriesInput)
+
+    try {
+      val numDatasets = 2000
+      val datasetsToGenerate = new collection.mutable.ArrayBuffer[Int]()
+      datasetsToGenerate ++= 1 to numDatasets
+      val datasetsBeingGenerated = new collection.mutable.ArrayBuffer[Future[Int]]()
+      val concurrency = 32
+
+      outPath.getFileSystem(sc.hadoopConfiguration).mkdirs(outPath)
+
+      while (datasetsToGenerate.nonEmpty || datasetsBeingGenerated.nonEmpty) {
+        // Check if any jobs are done
+        var i = 0
+        while (i < datasetsBeingGenerated.size) {
+          val datasetGenerationProcess = datasetsBeingGenerated(i)
+          try {
+            // Wait at most one second
+            Await.ready(datasetGenerationProcess, Duration.fromNanos(1E9))
+            datasetsBeingGenerated.remove(i)
+          } catch {
+            case _: TimeoutException | _: InterruptedException => i += 1
+          }
+        }
+        // Launch new jobs
+        while (datasetsToGenerate.nonEmpty && datasetsBeingGenerated.size < concurrency) {
+          val i = datasetsToGenerate.remove(datasetsToGenerate.size - 1)
+          datasetsBeingGenerated.append(Future {
+            val dataset = generateDataset(sc, i)
+            val datasetName = f"dataset-$i%04d"
+            // 1- Write the dataset to the output as a single file
+            val datasetFile = new Path(datasetsPath, datasetName + ".wkt.bz2")
+            if (!datasetFile.toString.startsWith("/dev/null") && !filesystem.isFile(datasetFile)) {
+              dataset.writeSpatialFile(datasetFile.toString, "wkt",
+                Seq(SpatialWriter.CompatibilityMode -> true, CSVFeatureWriter.FieldSeparator -> '\t',
+                  SpatialWriter.OverwriteOutput -> true))
+            }
+
+            // 2- Generate summary
+            val summaryPath = new Path(summariesPath, datasetName + "_summary.csv")
+            val globalSummary = if (!summaryPath.toString.startsWith("/dev/null") && !filesystem.isFile(summaryPath)) {
+              val tempSummaryPath = new Path(outPath, datasetName + "_summary_temp")
+              val (globalSummary, localSummaries) = GriddedSummary.computeForFeatures(dataset, 128, 128)
+              // Write the local summaries to the given input file
+              val localSummariesDF = GriddedSummary.createSummaryDataframe(globalSummary, localSummaries)
+              localSummariesDF.write
+                .option("delimiter", ",")
+                .option("header", true)
+                .mode(SaveMode.Overwrite)
+                .csv(tempSummaryPath.toString)
+              // Move the file out of the directory
+              val summaryFile = filesystem.listStatus(tempSummaryPath, new PathFilter {
+                override def accept(path: Path): Boolean = path.getName.startsWith("part")
+              })
+              filesystem.rename(summaryFile.head.getPath, summaryPath)
+              filesystem.delete(tempSummaryPath, true)
+              globalSummary
+            } else {
+              dataset.summary
+            }
+
+            if (summaryOutput != null) summaryOutput.synchronized {
+              summaryOutput.println(s"${datasetName},${globalSummary.getMinCoord(0)},${globalSummary.getMinCoord(1)}," +
+                s"${globalSummary.getMaxCoord(0)},${globalSummary.getMaxCoord(1)}," +
+                s"${globalSummary.numFeatures},${globalSummary.size}," +
+                s"${globalSummary.numPoints},${globalSummary.averageSideLength(0)},${globalSummary.averageSideLength(1)}," +
+                s"${globalSummary.averageArea}")
+            }
+
+            // 3- Draw an image of it
+            val imageFile = new Path(imagePath, datasetName + ".png")
+            if (!imageFile.toString.startsWith("/dev/null") && !filesystem.isFile(imageFile))
+              SingleLevelPlot.plotFeatures(dataset, 1024, 1024,
+                imageFile.toString, canvasMBR = globalMBR)
+
+            // 4- Run range queries
+            if (rangeQueries != null) {
+              // Index the dataset
+              val indexedDataset = dataset.spatialPartition(classOf[RSGrovePartitioner]).persist()
+              // Select all the matching queries
+              val queries = rangeQueries.filter(s"datasetName='${datasetName}'").collect()
+              val geometryFactory = new GeometryFactory()
+              var iQuery: Int = 0
+              queries.foreach(row => {
+                val x1 = row.getAs[Double]("minX")
+                val y1 = row.getAs[Double]("minY")
+                val x2 = row.getAs[Double]("maxX")
+                val y2 = row.getAs[Double]("maxY")
+                val numMBRTests = sc.longAccumulator
+                val query = geometryFactory.toGeometry(new Envelope(x1, x2, y1, y2))
+                val resultSize = indexedDataset.rangeQuery(query, numMBRTests).count()
+                queriesOutput.synchronized {
+                  queriesOutput.println(Array(datasetName, iQuery, query.getArea,
+                    query.getEnvelopeInternal.intersection(globalSummary.toJTSEnvelope).getArea,
+                    resultSize, "--", numMBRTests.value).mkString(";"))
+                }
+                iQuery += 1
+              })
+            }
+            i
+          })
+        }
+      }
+    }
+    finally {
+      spark.stop()
+      if (queriesOutput != null)
+        queriesOutput.close()
+      if (summaryOutput != null)
+        summaryOutput.close()
+    }
+
+  }
+
+  def generateDataset(sc: SparkContext, i: Int): SpatialRDD = {
     import edu.ucr.cs.bdlab.beast._
-    import edu.ucr.cs.bdlab.beast.generator.{BitDistribution, DiagonalDistribution, DistributionType, GaussianDistribution, ParcelDistribution, SierpinskiDistribution, UniformDistribution}
-    import edu.ucr.cs.bdlab.beast.geolite.EnvelopeNDLite
-    import edu.ucr.cs.bdlab.beast.io.{CSVFeatureWriter, SpatialWriter}
-
-    import scala.concurrent.ExecutionContext.Implicits.global
-    import scala.concurrent.duration._
-    import scala.concurrent.{Await, Future}
-    import scala.util.Random
-
     def randomDouble(random: Random, range: Array[Double]) = {
       random.nextDouble() * (range(1) - range(0)) + range(0)
     }
@@ -66,114 +231,40 @@ object GenerateRandomData {
     // For parcel
     val splitRanges = Array(0.1, 0.5)
     val dither = Array(0.1, 0.5)
-    val numDatasets = 2000
-    val datasetsToGenerate = new collection.mutable.ArrayBuffer[Int]()
-    datasetsToGenerate ++= 1 to numDatasets
-    val datasetsBeingGenerated = new collection.mutable.ArrayBuffer[Future[Int]]()
-    val globalMBR = new EnvelopeNDLite(2, 0, 0, 10, 10)
-    val concurrency = 32
 
-    outPath.getFileSystem(sc.hadoopConfiguration).mkdirs(outPath)
-
-    println("dataset,x1,y1,x2,y2,num_features,size,num_points,avg_area,avg_side_length_0,avg_side_length_1")
-
-    while (datasetsToGenerate.nonEmpty || datasetsBeingGenerated.nonEmpty) {
-      // Check if any jobs are done
-      var i = 0
-      while (i < datasetsBeingGenerated.size) {
-        val datasetGenerationProcess = datasetsBeingGenerated(i)
-        try {
-          // Wait at most one second
-          Await.ready(datasetGenerationProcess, Duration.fromNanos(1E9))
-          datasetsBeingGenerated.remove(i)
-        } catch {
-          case _: TimeoutException | _: InterruptedException => i += 1
-        }
-      }
-      // Launch new jobs
-      while (datasetsToGenerate.nonEmpty && datasetsBeingGenerated.size < concurrency) {
-        val i = datasetsToGenerate.remove(datasetsToGenerate.size - 1)
-        datasetsBeingGenerated.append(Future {
-          val random = new Random(i)
-          val datasetName = f"dataset-$i%04d"
-          val filesystem = outPath.getFileSystem(sc.hadoopConfiguration)
-          val distribution: DistributionType = if (i <= 1000) UniformDistribution
-          else if (i <= 1200) DiagonalDistribution
-          else if (i <= 1400) GaussianDistribution
-          else if (i <= 1600) ParcelDistribution
-          else if (i <= 1800) BitDistribution
-          else if (i <= 2000) SierpinskiDistribution
-          else null
-          val cardinality = random.nextInt(cardinalities(1) - cardinalities(0)) + cardinalities(0)
-          val mbrWidth = randomDouble(random, Array(1.0, globalMBR.getSideLength(0)))
-          val mbrHeight = randomDouble(random, Array(1.0, globalMBR.getSideLength(1)))
-          val x1 = randomDouble(random, Array(globalMBR.getMinCoord(0), globalMBR.getMaxCoord(0) - mbrWidth))
-          val y1 = randomDouble(random, Array(globalMBR.getMinCoord(1), globalMBR.getMaxCoord(1) - mbrHeight))
-          val datasetMBR = new EnvelopeNDLite(2, x1, y1, x1 + mbrWidth, y1 + mbrHeight)
-          val generator = sc.generateSpatialData.mbr(datasetMBR)
-            .config(UniformDistribution.MaxSize, s"${randomDouble(random, boxSizes) / (mbrWidth max mbrHeight)}")
-            .config(UniformDistribution.NumSegments, s"${random.nextInt(numSegments(1) - numSegments(0)) + numSegments(0)}")
-            .config(UniformDistribution.GeometryType, "polygon")
-            .config(SpatialGenerator.Seed, i)
-            .distribution(distribution)
-          distribution match {
-            case BitDistribution =>
-              generator.config(BitDistribution.Digits, 12)
-                .config(BitDistribution.Probability, randomDouble(random, probabilities))
-            case ParcelDistribution =>
-              generator.config(ParcelDistribution.SplitRange, randomDouble(random, splitRanges))
-                .config(ParcelDistribution.Dither, randomDouble(random, dither))
-            case DiagonalDistribution =>
-              generator.config(DiagonalDistribution.Buffer, randomDouble(random, buffers))
-                .config(DiagonalDistribution.Percentage, randomDouble(random, percentages))
-            case _ => // Nothing need to be done but the case has to be added to avoid no match exception
-          }
-          val dataset: SpatialRDD = generator.generate(cardinality)
-          // 1- Write the dataset to the output as a single file
-          val datasetFile = new Path(outPath, datasetName+".wkt.bz2")
-          if (!filesystem.isFile(datasetFile)) {
-            dataset.writeSpatialFile(datasetFile.toString, "wkt",
-              Seq(SpatialWriter.CompatibilityMode -> true, CSVFeatureWriter.FieldSeparator -> '\t',
-                SpatialWriter.OverwriteOutput -> true))
-          }
-
-          // 2- Generate summary
-          val summaryPath = new Path(outPath, datasetName+"_summary.csv")
-          val globalSummary =
-          if (!filesystem.isFile(summaryPath)) {
-            val tempSummaryPath = new Path(outPath, datasetName + "_summary_temp")
-            val (globalSummary, localSummaries) = GriddedSummary.computeForFeatures(dataset, 128, 128)
-            // Write the local summaries to the given input file
-            val localSummariesDF = GriddedSummary.createSummaryDataframe(globalSummary, localSummaries)
-            localSummariesDF.write
-              .option("delimiter", ",")
-              .option("header", true)
-              .mode(SaveMode.Overwrite)
-              .csv(tempSummaryPath.toString)
-            // Move the file out of the directory
-            val summaryFile = filesystem.listStatus(tempSummaryPath, new PathFilter {
-              override def accept(path: Path): Boolean = path.getName.startsWith("part")
-            })
-            filesystem.rename(summaryFile.head.getPath, summaryPath)
-            filesystem.delete(tempSummaryPath, true)
-            globalSummary
-          } else {
-            dataset.summary
-          }
-          println(s"${datasetName},${x1},${y1},${x1+mbrWidth},${y1+mbrHeight},${cardinality},${globalSummary.size}," +
-            s"${globalSummary.numPoints},${globalSummary.averageSideLength(0)},${globalSummary.averageSideLength(1)}," +
-            s"${globalSummary.averageArea}")
-
-          // 3- Draw an image of it
-          val imageFile = new Path(outPath, datasetName + ".png")
-          if (!filesystem.isFile(imageFile))
-            SingleLevelPlot.plotFeatures(dataset, 1024, 1024,
-              imageFile.toString, canvasMBR = globalMBR)
-          i
-        })
-      }
+    val random = new Random(i)
+    val distribution: DistributionType = if (i <= 1000) UniformDistribution
+    else if (i <= 1200) DiagonalDistribution
+    else if (i <= 1400) GaussianDistribution
+    else if (i <= 1600) ParcelDistribution
+    else if (i <= 1800) BitDistribution
+    else if (i <= 2000) SierpinskiDistribution
+    else null
+    val cardinality = random.nextInt(cardinalities(1) - cardinalities(0)) + cardinalities(0)
+    val mbrWidth = randomDouble(random, Array(1.0, globalMBR.getSideLength(0)))
+    val mbrHeight = randomDouble(random, Array(1.0, globalMBR.getSideLength(1)))
+    val x1 = randomDouble(random, Array(globalMBR.getMinCoord(0), globalMBR.getMaxCoord(0) - mbrWidth))
+    val y1 = randomDouble(random, Array(globalMBR.getMinCoord(1), globalMBR.getMaxCoord(1) - mbrHeight))
+    val datasetMBR = new EnvelopeNDLite(2, x1, y1, x1 + mbrWidth, y1 + mbrHeight)
+    val generator = sc.generateSpatialData.mbr(datasetMBR)
+      .config(UniformDistribution.MaxSize, s"${randomDouble(random, boxSizes) / (mbrWidth max mbrHeight)}")
+      .config(UniformDistribution.NumSegments, s"${random.nextInt(numSegments(1) - numSegments(0)) + numSegments(0)}")
+      .config(UniformDistribution.GeometryType, "polygon")
+      .config(SpatialGenerator.Seed, i)
+      .distribution(distribution)
+    distribution match {
+      case BitDistribution =>
+        generator.config(BitDistribution.Digits, 12)
+          .config(BitDistribution.Probability, randomDouble(random, probabilities))
+      case ParcelDistribution =>
+        generator.config(ParcelDistribution.SplitRange, randomDouble(random, splitRanges))
+          .config(ParcelDistribution.Dither, randomDouble(random, dither))
+      case DiagonalDistribution =>
+        generator.config(DiagonalDistribution.Buffer, randomDouble(random, buffers))
+          .config(DiagonalDistribution.Percentage, randomDouble(random, percentages))
+      case _ => // Nothing need to be done but the case has to be added to avoid no match exception
     }
-
+    generator.generate(cardinality)
   }
 
 }
