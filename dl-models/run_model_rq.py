69deeb7318054e4a1f3aba6d05e40070bd599963
albertounivr <alberto.belussi@univr.it>
2021-11-16 11:10:29 +0100

    Added the function for computing mape error for values different from zero and more
    Added the function for computing mape error for values different from zero in the result y vs predict values. Modified the call of the function gen_rq_input_from_file including the file containing the cardinality of the dataset, which is necessary for the computation of the selectivity of the range query instead of its cardinality.

--- dl-models/run_model_rq.py
+++ dl-models/run_model_rq.py
@@ -1,4 +1,5 @@
 # loading the models for local and global embedding
+import numpy as np
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers, losses
@@ -10,7 +11,7 @@ def run_model():
 	local_enc = keras.models.load_model('model/model_3072_CNNDense_newDatasets_SMLG')
 	global_enc = keras.models.load_model('model/model_2048_CNNDense_newDatasets_SMLG_global')
 
-	x, y = gt.gen_rq_input_from_file(local_enc, global_enc, 'rq/rq_newDatasets.csv', 'mbr/mbr_alldatasets.csv', 'rq/rq_result.csv', 'histograms/new_datasets', ',', ';')
+	x, y = gt.gen_rq_input_from_file(local_enc, global_enc, 'rq/rq_newDatasets.csv', 'mbr/mbr_alldatasets.csv', 'dataset-summaries.csv', 'rq/rq_result.csv', 'histograms/new_datasets', ',', ';')
 
 	#
 	print("Inizilizing model DENSE local...")
@@ -18,9 +19,35 @@ def run_model():
 	model_rq = m.RQ_sel_DENSE(32,32,7,filter)
 	model_rq.compile(optimizer='adam', loss=losses.MeanSquaredError())
 	print("Splitting training and test set...")
-	X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
+	X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, shuffle=true)
 
 	# training
 	print("Training model...")
 	history = model_rq.fit(X_train, y_train, batch_size=16, epochs=30, shuffle=True, validation_data=(X_test, y_test))
 	return x, y, X_train, X_test, y_train, y_test, model_rq, history
+
+def mape_error_zero (y, predict):
+	delta_zero = 0.0
+	zero = 0
+	non_zero = 0
+	delta = 0.0
+	p = 0
+	for i in range(y.shape[0]):
+		if (y[i] == 0.0):
+			zero += 1
+			delta_zero += predict[i]
+			if (zero < 50):
+				print(y[i], predict[i])
+		else:
+			non_zero += 1
+			delta += abs(y[i] - predict[i])/y[i]
+			if (p < 50 and abs(y[i] - predict[i])/y[i] > 50):
+				print(y[i], predict[i])
+				p += 1
+	return delta/non_zero, non_zero, delta_zero/zero, zero
+def zero_one (y):
+	y_zero_one = np.zeros((y.shape[0]))
+	for i in range(y.shape[0]):
+		if (y[i] > 0.0):
+			y_zero_one[i] = 1
+	return y_zero_one
