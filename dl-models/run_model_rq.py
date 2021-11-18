5a96c065eaf7f89459a12bc9c8809da3e86f7216
albertounivr <alberto.belussi@univr.it>
2021-11-18 15:46:30 +0100

    Fixed some errors

--- dl-models/run_model_rq.py
+++ dl-models/run_model_rq.py
@@ -3,15 +3,15 @@ import numpy as np
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers, losses
-import gen_inputRQ_test as gt
+import generate_input_RQ as gt
 import myModel_RQ_selectivity as m
 from sklearn.model_selection import train_test_split
 
 def run_model():
 	local_enc = keras.models.load_model('model/model_3072_CNNDense_newDatasets_SMLG')
-	global_enc = keras.models.load_model('model/model_2048_CNNDense_newDatasets_SMLG_global')
+	global_enc = keras.models.load_model('model/model_2048_CNNDense_newDatasets_SMLG_global_new')
 
-	x, y = gt.gen_rq_input_from_file(local_enc, global_enc, 'rq/rq_newDatasets.csv', 'mbr/mbr_alldatasets.csv', 'dataset-summaries.csv', 'rq/rq_result.csv', 'histograms/new_datasets', ',', ';')
+	x, y = gt.gen_rq_input_from_file(local_enc, global_enc, 'rq/rq_newDatasets.csv', 'mbr/mbr_alldatasets.csv', 'rq/rq_result.csv', 'dataset-summaries.csv', 'histograms/new_datasets/', ';')
 
 	#
 	print("Inizilizing model DENSE local...")
@@ -19,11 +19,11 @@ def run_model():
 	model_rq = m.RQ_sel_DENSE(32,32,7,filter)
 	model_rq.compile(optimizer='adam', loss=losses.MeanSquaredError())
 	print("Splitting training and test set...")
-	X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, shuffle=true)
+	X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
 
 	# training
 	print("Training model...")
-	history = model_rq.fit(X_train, y_train, batch_size=16, epochs=30, shuffle=True, validation_data=(X_test, y_test))
+	history = model_rq.fit(X_train, y_train, batch_size=8, epochs=0, shuffle=True, validation_data=(X_test, y_test))
 	return x, y, X_train, X_test, y_train, y_test, model_rq, history
 
 def mape_error_zero (y, predict):
