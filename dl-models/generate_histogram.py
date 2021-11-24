Fixed bugs in normalization functions: use nor_g_ab

dl-models/generate_histogram.py
@@ -575,12 +575,14 @@ def nor_a_ab(a,c,min,max):
 		a_norm = np.log(1 + c * a)
 	else:
 		a_norm = a
-	minimum = math.log(1+c*min)
-	if (min == -1):
-		minimum = np.amin(a_norm, axis=(0, 1, 2))
-	maximum = math.log(1+c*max)
-	if (max == -1):
-		maximum = np.amax(a_norm, axis=(0, 1, 2))
+	min = np.array(min)
+	minimum = np.log(1+c*min)
+	#if (min == -1):
+	#	minimum = np.amin(a_norm, axis=(0, 1, 2))
+	max= np.array(max)
+	maximum = np.log(1+c*max)
+	#if (max == -1):
+	#	maximum = np.amax(a_norm, axis=(0, 1, 2))
 	for z_dim in range(a_norm.shape[3]):
 		a_norm[:,:,:,z_dim]= ( a_norm[:,:,:,z_dim] - minimum[z_dim] ) /\
 								( maximum[z_dim]-minimum[z_dim] )
@@ -647,39 +649,28 @@ def nor_y_ab(y,c,min,max):
 	print("MAX: if (c>0) them log(1+c*max) else max: ", maximum)
 	return y_norm
 #
-def nor_g_ab(g,c,min,max):
+def nor_g_ab(hist,c,min,max):
 	# c = 0: normalization MIN-MAX
 	# c > 0: each value x is converted by applying the logarithic function new_x = log(1+c*x)
 	# min = -1: the minimum is computed
 	# max = -1: the maximum is computed
 	print("Normalizing g with AB approach...")
-	minimum = 0.0
-	maximum = 1.0
-	if (c > 0):
-		g_norm = np.log(1 + c * g)
-	else:
-		g_norm = g
-	if (min == -1):
-		minimum = np.amin(g_norm, axis=(0, 1, 2))
-	else:
-		if (c > 0):
-			minimum = math.log(1+c*min)
-		else:
-			minimum = min
-	if (max == -1):
-		maximum = np.amax(g_norm, axis=(0, 1, 2))
-	else:
-		if (c > 0):
-			maximum = math.log(1+c*max)
-		else:	
-			maximum = max
-	for z_dim in range(g_norm.shape[3]):
-		g_norm[:,:,:,z_dim]= ( g_norm[:,:,:,z_dim] - minimum[z_dim] ) /\
-							( maximum[z_dim] - minimum[z_dim] )
-	#
-	print("MIN: if (c>0) them log(1+c*min) else min: ", minimum)
-	print("MAX: if (c>0) them log(1+c*max) else max: ", maximum)
-	return g_norm
+	if (c):
+		hist = np.log(1 + c * hist)
+		min = np.log(1 + c * np.array(min)) if (type(min) == list or min != -1) else min
+		max = np.log(1 + c * np.array(max)) if (type(max) == list or max != -1) else max
+	minimum = np.amin(hist, axis=(0, 1, 2)) if (type(min) == int and min == -1) else min
+	maximum = np.amax(hist, axis=(0, 1, 2)) if (type(max) == int and max == -1) else max
+
+	if len(hist.shape) == 3:
+		return (hist - minimum) / (maximum - minimum)
+	
+	for z_dim in range(hist.shape[3]):
+		hist[:, :, :, z_dim] = (hist[:, :, :, z_dim] - minimum[z_dim]) / (maximum[z_dim] - minimum[z_dim])
+	print("MIN: if (c>0) then log(1+c*min) else min: ", minimum)
+	print("MAX: if (c>0) then log(1+c*max) else max: ", maximum)
+	return hist
+#
 def denorm_y_ab(y_nor, c, min, max):
 	print("DeNoromalizing y..	.")
 	min_log = math.log(1+c*min)