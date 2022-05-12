e3a83f4f10bcfc16ff2f46619b0a16bab4c8e965
albertounivr <alberto.belussi@univr.it>
2022-05-12 18:51:58 +0200

    added real datasets to the initial set of data points

--- dl-models/load_datasets.py
+++ dl-models/load_datasets.py
@@ -4,11 +4,18 @@ import numpy as np
 import time
 import plot as p
 from sklearn.model_selection import train_test_split
+# load HISTOGRAMS real data
+def load_hist_real ():
+	a_it, g_it = m.gh.gen_input_from_file(128,128,6,'histograms/real_datasets/italy','mbr/mbr_italy.csv',0,'')
+	print('Histograms Italy loaded!')
+	m.np.save('histograms_loc_it',a_it)
+	m.np.save('histograms_glo_it',g_it)
 # load HISTOGRAMS
 def load_hist ():
 	time_exe = np.zeros((2,), dtype=float)
 	print('Loading histograms...')
 	time0 = time.time()
+	a_real, g_real = m.gh.gen_input_from_file(128,128,6,'histograms/real_datasets','mbr/mbr_alldatasets.csv',0,'')
 	a_new, g_new = m.gh.gen_input_from_file(128,128,6,'histograms/new_datasets','mbr/mbr_alldatasets.csv',0,'')
 	a_large, g_large = m.gh.gen_input_from_file(128,128,6,'histograms/large_datasets','mbr/mbr_alldatasets.csv',0,'')
 	a_medium, g_medium = m.gh.gen_input_from_file(128,128,6,'histograms/medium_datasets','mbr/mbr_alldatasets.csv',0,'_m')
@@ -27,18 +34,18 @@ def load_hist ():
 	a_gap.shape
 #
 	print('Concatenating histograms...')
-	a_tot = m.np.concatenate((a_new,a_large,a_gap,a_medium,a_small))
-	g_tot = m.np.concatenate((g_new,g_large,g_gap,g_medium,g_small))
+	a_tot = m.np.concatenate((a_real,a_new,a_large,a_gap,a_medium,a_small))
+	g_tot = m.np.concatenate((g_real,g_new,g_large,g_gap,g_medium,g_small))
 #
 	a_tot.shape
 	g_tot.shape
 #
-	m.np.save('histograms_loc_tot',a_tot)
-	m.np.save('histograms_glo_tot',g_tot)
+	m.np.save('histograms_loc_tot_real',a_tot)
+	m.np.save('histograms_glo_tot_real',g_tot)
 	print('Times: ',time_exe)
-def nor_train(file_a, file_g, latent_dim, net_type, loc, glo):
-	a_tot = m.np.load('histograms_loc_tot.npy')
-	g_tot = m.np.load('histograms_glo_tot.npy')
+def nor_train(file_a, file_g, latent_dim, net_type, loc, glo, f1, f2):
+	a_tot = m.np.load(file_a)
+	g_tot = m.np.load(file_g)
 	time_exe = np.zeros((3,), dtype=float)
 	time0 = time.time()
 	# normalization
@@ -60,14 +67,22 @@ def nor_train(file_a, file_g, latent_dim, net_type, loc, glo):
 	#
 	print('Training autoencoder...')
 	# try:  48 96 192 384, 480, 768, 1536, 3072
-	latent_dim = 48
+	latent_dim = 1536
 	# DENSE, LOCAL YES, GLOBAL NO - AUTOENCODER = m.auto_encoder(0,1,0
-	autoenc, a_train_train, a_train_val =  m.auto_encoder(net_type,loc,glo,128,128,6,latent_dim,2,a_train,g_train)
+	# CNN, LOCAL YES, GLOBAL NO - AUTOENCODER = m.auto_encoder(1,1,0
+	autoenc, a_train_train, a_train_val =  m.auto_encoder(net_type,loc,glo,128,128,6,latent_dim,2,f1,f2,a_train,g_train)
 	print("A_TEST: ", a_test[0,0,0,0]," ",a_test[10,0,0,0])
 	time_exe[1] = time.time()-time0
 	print('Time for training: ',time_exe[1])
 	time0 = time.time()
-
+	autoenc.summary()
+	a_train = np.array([1, 2, 3])
+	g_train = np.array([4, 5, 6])
+	a_tot = np.array([1, 2, 3])
+	g_tot = np.array([4, 5, 6])
+	a_tot_norm_log = np.array([1, 2, 3])
+	g_tot_norm_log = np.array([1, 2, 3])
+	print('All arrays deleted!')
 	enc_a_test = autoenc.encoder(a_test).numpy()
 	dec_a_test = autoenc.decoder(enc_a_test).numpy()
 	enc_a_test_reshape = m.np.reshape(enc_a_test, (-1,16,int(latent_dim/(3*16)),3))
@@ -80,7 +95,7 @@ def nor_train(file_a, file_g, latent_dim, net_type, loc, glo):
 	print("A_TEST: ", a_test[0,0,0,0]," ",a_test[10,0,0,0])
 	print("A_TEST_DEN: ", a_test_denor[0,0,0,0]," ",a_test_denor[10,0,0,0])
 	print('Plotting...')
-	p.plot_h6_mix_neg_emb(a_test, dec_a_test, enc_a_test_reshape, 0, 10, 'ae_dense_384')
+	p.plot_h6_mix_neg_emb(a_test, dec_a_test, enc_a_test_reshape, 0, 10, 'ae_CNN_768')
 	return autoenc, a_test, dec_a_test, a_test_denor, dec_a_test_denor, enc_a_test_reshape
 #
 	#autoenc_g_tot_log, g_tot_train, g_tot_test =  m.auto_encoder(1,0,1,128,128,6,3,2,a_tot_norm_log,g_tot_norm_log,b_tot,r_tot)
