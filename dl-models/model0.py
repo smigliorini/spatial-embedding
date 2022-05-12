a7c332a48fc75f9087e283e5e6af3e78e5cb8034
albertounivr <alberto.belussi@univr.it>
2022-05-12 18:52:57 +0200

    new parameters f1 e f2

--- dl-models/model0.py
+++ dl-models/model0.py
@@ -45,10 +45,10 @@ def create_h(n,dimx,dimy,dimz,norm_type,distr):
 # model definition
 
 # Autoencoder
-def auto_encoder(type,loc,glo,dimx,dimy,dimz,ldim_loc,ldim_glo,a,g):
+def auto_encoder(type,loc,glo,dimx,dimy,dimz,ldim_loc,ldim_glo,f1,f2,a,g):
 	# type: 0=DENSE, 1=CNN
-	# loc: 1=generate local autoencored, 0=local autoencoder is not generated
-	# glo: 1=generate global autoencored, 0=global autoencoder is not generated
+	# loc: 1=generate local autoencoder, 0=local autoencoder is not generated
+	# glo: 1=generate global autoencoder, 0=global autoencoder is not generated
 	# latent_dim is equal to the size of the embedding representation of the histogram for DENSE network
 	# while for CNN it is equal to the number of values in each cell of the embedding, thus the size of
 	# the embedding is obtained by the formula: dimx/4 * dimy/4 * latent_dim
@@ -57,13 +57,16 @@ def auto_encoder(type,loc,glo,dimx,dimy,dimz,ldim_loc,ldim_glo,a,g):
 		if (type == 0):
 			print("Embedding dim: ", latent_dim)
 			print("Inizilizing Autoencoder DENSE local...")
-			ae_loc = enc.Autoencoder_local(latent_dim,dimx,dimy,dimz)
+			ae_loc = enc.Autoencoder_local(f1,f2,latent_dim,dimx,dimy,dimz)
 		else:
-			print("Embedding dim: ", latent_dim*dimx/4*dimy/4)
+			#print("Embedding dim: ", latent_dim*dimx/4*dimy/4)
+			print("Embedding dim: ", latent_dim)
 			print("Inizilizing Autoencoder CNN local...")
-			ae_loc = enc.AutoencoderCNN_local(latent_dim,dimx,dimy,dimz)
+			ae_loc = enc.AutoencoderCNN_local(latent_dim,dimx,dimy,dimz,f1,f2)
+		#
 		ae_loc.compile(optimizer='adam', loss=losses.MeanSquaredError())
-		#ae_loc.compile(optimizer='adam', loss='binary_crossentropy')
+		# ae_loc.compile(optimizer='adam', loss=losses.MeanAbsolutePercentageError())
+		# ae_loc.compile(optimizer='adam', loss='binary_crossentropy')
 
 		# splitting train and test 0.2
 		print("Splitting training and test set...")
@@ -71,7 +74,7 @@ def auto_encoder(type,loc,glo,dimx,dimy,dimz,ldim_loc,ldim_glo,a,g):
 		X_train_hist, X_test_hist = train_test_split(a, test_size=0.2)
 		# training
 		print("Training Autoencoder local...")
-		ae_loc.fit(X_train_hist, X_train_hist, batch_size=16, epochs=30, shuffle=True, validation_data=(X_test_hist, X_test_hist))
+		ae_loc.fit(X_train_hist, X_train_hist, batch_size=16, epochs=50, shuffle=True, validation_data=(X_test_hist, X_test_hist))
 	else:
 		print("Skip local autoencoder")
 	if (glo == 1):
@@ -84,15 +87,16 @@ def auto_encoder(type,loc,glo,dimx,dimy,dimz,ldim_loc,ldim_glo,a,g):
 			print("Embedding dim: ", latent_dim*dimx/4*dimy/4)
 			print("Inizilizing Autoencoder CNN global...")
 			ae_glo = enc.AutoencoderCNN_global(latent_dim,dimx,dimy)
+		# ae_glo.compile(optimizer='adam', loss='binary_crossentropy')
 		ae_glo.compile(optimizer='adam', loss=losses.MeanSquaredError())
-		#ae_glo.compile(optimizer='adam', loss='binary_crossentropy')
+		# ae_glo.compile(optimizer='adam', loss=losses.MeanAbsolutePercentageError())
 		# splitting train and test 0.2
 		print("Splitting training and test set...")
 		# global histograms
 		X_train_global, X_test_global = train_test_split(np.reshape(g,(g.shape[0],g.shape[1],g.shape[2],1)), test_size=0.2)
 		# training
 		print("Training Autoencoder global...")
-		ae_glo.fit(X_train_global, X_train_global, batch_size=16, epochs=30, shuffle=True, validation_data=(X_test_global, X_test_global))
+		ae_glo.fit(X_train_global, X_train_global, batch_size=16, epochs=50, shuffle=True, validation_data=(X_test_global, X_test_global))
 	else:
 		print("Skip global autoencoder")
 	if (loc == 1):
