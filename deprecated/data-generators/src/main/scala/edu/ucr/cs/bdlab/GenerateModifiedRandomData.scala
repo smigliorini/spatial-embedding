56b06f58d3e24e9fd21c46a096389c15e29c62a7
Ahmed Eldawy <eldawy@ucr.edu>
2023-02-08 21:36:57 -0800

    Minor fix

--- deprecated/data-generators/src/main/scala/edu/ucr/cs/bdlab/GenerateModifiedRandomData.scala
+++ deprecated/data-generators/src/main/scala/edu/ucr/cs/bdlab/GenerateModifiedRandomData.scala
@@ -185,7 +185,7 @@ object GenerateModifiedRandomData {
             if (datasetDescriptors != null) {
               datasetDescriptors.synchronized {
                 val info: Row = dataset.asInstanceOf[RandomSpatialRDD].descriptor
-                val values: Array[Object] = datasetName +: Row.unapplySeq(info).toArray
+                val values: Seq[Any] = datasetName +: Row.unapplySeq(info).get
                 val schema: Seq[StructField] = StructField("name", StringType) +: info.schema
                 val finalRow = new GenericRowWithSchema(values.toArray, StructType(schema))
                 datasetDescriptors.println(finalRow.json)
